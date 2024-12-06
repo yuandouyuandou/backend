c38ac4c44c46edf660866d7ceb36cd6094fc54e6
yuan <zzhao01@uw.edu>
2024-12-05 17:08:46 -0800

    change path

--- course_recommendation/recommendations/views.py
+++ course_recommendation/recommendations/views.py
@@ -74,7 +74,7 @@ def recommend_courses(request):
             )
 
             # Load pre-trained weights
-            trainer.model.load_weights('sasrec_weights')
+            trainer.model.load_weights('/home/ubuntu/backend/course_recommendation/recommendations/sasrec_weights')
             recommendations = trainer.recommend(student_data, course_data, num_recommendations=10)
 
             recommended_course_ids = []
